7e58ded096f1e0cff5ecfe2e3ecc98506a0f2931
ALK <alko@istperm.ru>
2021-06-23 12:06:56 +0500

    +Rune<->Char conversions

--- akutils.go
+++ akutils.go
@@ -1,4 +1,4 @@
-package akutils
+package utils
 
 import (
 	"fmt"
@@ -42,4 +42,76 @@ func Dump(s interface{}, lvl int) string {
 	}
 
 	return res
+}
+
+func BoolToString(v bool, s1, s0 string) string {
+	if v {
+		return s1
+	} else {
+		return s0
+	}
+}
+
+type RuneChar struct {
+	RuneUp rune
+	RuneLo rune
+	CharUp byte
+	CharLo byte
+}
+
+var RuneChars = []RuneChar{
+	{'А', 'а', 0xC0, 0xE0},
+	{'Б', 'б', 0xC1, 0xE1},
+	{'В', 'в', 0xC2, 0xE2},
+	{'Г', 'г', 0xC3, 0xE3},
+	{'Д', 'д', 0xC4, 0xE4},
+	{'Е', 'е', 0xC5, 0xE5},
+	{'Ж', 'ж', 0xC6, 0xE6},
+	{'З', 'з', 0xC7, 0xE7},
+	{'И', 'и', 0xC8, 0xE8},
+	{'Й', 'й', 0xC9, 0xE9},
+	{'К', 'к', 0xCA, 0xEA},
+	{'Л', 'л', 0xCB, 0xEB},
+	{'М', 'м', 0xCC, 0xEC},
+	{'Н', 'н', 0xCD, 0xED},
+	{'О', 'о', 0xCE, 0xEE},
+	{'П', 'п', 0xCF, 0xEF},
+	{'Р', 'р', 0xD0, 0xF0},
+	{'С', 'с', 0xD1, 0xF1},
+	{'Т', 'т', 0xD2, 0xF2},
+	{'У', 'у', 0xD3, 0xF3},
+	{'Ф', 'ф', 0xD4, 0xF4},
+	{'Х', 'х', 0xD5, 0xF5},
+	{'Ц', 'ц', 0xD6, 0xF6},
+	{'Ч', 'ч', 0xD7, 0xF7},
+	{'Ш', 'ш', 0xD8, 0xF8},
+	{'Щ', 'щ', 0xD9, 0xF9},
+	{'Ъ', 'ъ', 0xDA, 0xFA},
+	{'Ы', 'ы', 0xDB, 0xFB},
+	{'Ь', 'ь', 0xDC, 0xFC},
+	{'Э', 'э', 0xDD, 0xFD},
+	{'Ю', 'ю', 0xDE, 0xFE},
+	{'Я', 'я', 0xDF, 0xFF},
+}
+
+func RuneToChar(r rune) byte {
+	for _, rc := range RuneChars {
+		switch r {
+		case rc.RuneUp, rc.RuneLo:
+			return rc.CharUp
+		}
+	}
+	return byte(r)
+}
+
+func CharToRune(c byte) rune {
+	for _, rc := range RuneChars {
+		switch c {
+		case rc.CharUp:
+			return rc.RuneUp
+		case rc.CharLo:
+			return rc.RuneLo
+		}
+	}
+	return rune(c)
 }
